4c62e9d8406df78c719e39347b470b044ad1118d
Dmytro Puzyrenko <dmytro.puzyrenko@infopulse.com>
2020-10-22 01:21:04 +0300

    add search field

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,73 @@
 <template>
-	<v-app>
-		<v-card
-						color="grey lighten-4"
-						flat
-						tile
-		>
-			<v-toolbar dense>
-				<v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app>
+        <v-card
+                color="grey lighten-4"
+                flat
+                tile
+        >
+            <v-toolbar dense>
 
-				<v-toolbar-title>Title</v-toolbar-title>
+                <v-toolbar-title>Weather App</v-toolbar-title>
 
-				<v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-				<v-btn icon>
-					<v-icon>mdi-magnify</v-icon>
-				</v-btn>
 
-				<v-btn icon>
-					<v-icon>mdi-heart</v-icon>
-				</v-btn>
+                <a href="https://github.com/dmssk" target="_blank" title="dev">
+                    <v-btn icon>
+                        <v-icon>mdi-github</v-icon>
+                    </v-btn>
+                </a>
 
-				<v-btn icon>
-					<v-icon>mdi-dots-vertical</v-icon>
-				</v-btn>
-			</v-toolbar>
-		</v-card>
+            </v-toolbar>
+        </v-card>
 
-		<v-main class="mt-5 px-5">
-      <v-weather-card/>
-		</v-main>
-	</v-app>
+        <v-main class="mt-5 pa-5">
+            <v-tabs>
+                <v-tab>Item One</v-tab>
+                <v-tab>Item Two</v-tab>
+                <v-tab>Item Three</v-tab>
+            </v-tabs>
+            <v-form class="mt-5" @submit.prevent="searchCity">
+                <v-text-field
+                        v-model="city"
+                        label="Find your sity"
+                        solo
+                ></v-text-field>
+            </v-form>
+            <v-weather-card/>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 
-  import VWeatherCard from "./components/VWeatherCard";
+    import VWeatherCard from "./components/VWeatherCard";
 
-  export default {
-    name: 'App',
+    export default {
+        name: 'App',
 
-    components: {
-      VWeatherCard
-    },
+        components: {
+            VWeatherCard
+        },
 
-    data: () => ({
-      //
-    }),
-  };
+        data: () => ({
+            city: ''
+        }),
+        methods: {
+            searchCity() {
+                console.log(this.city)
+            }
+        }
+    };
 </script>
+
+<style lang="scss">
+    a {
+        text-decoration: none;
+    }
+
+    .v-main {
+        max-width: 600px;
+        margin: 0 auto;
+    }
+</style>
